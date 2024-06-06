c96fdd9f1d307646e957a982ef7f1f83032bd803
Olleh Spirit <Olleh.Spirit@gmail.com>
2024-06-06 14:39:05 +0300

    add about section

---
+++ JavaScript/aboutAnimation.js
@@ -0,0 +1,33 @@
+const p1_About = document.querySelectorAll('.about p:nth-child(1)')
+
+const p1_About_Observer = new IntersectionObserver((elements) => {
+    elements.forEach(element => {
+        if (element.isIntersecting) {
+            element.target.classList.add('letterSpacingAnimation')
+            document.querySelectorAll('.listHeader div').forEach(element=>{
+                element.classList.remove('active')
+            })
+            document.querySelectorAll('.listHeader div:nth-child(2)')[0].classList.add('active')
+        }
+        else {
+            document.querySelectorAll('.listHeader div:nth-child(2)')[0].classList.remove('active')
+        }
+    })
+})
+
+p1_About.forEach(element => p1_About_Observer.observe(element))
+
+
+
+
+const p2_About = document.querySelectorAll('.about p:nth-child(2)')
+
+const p2_About_Observer = new IntersectionObserver((elements) => {
+    elements.forEach(element => {
+        if (element.isIntersecting)
+            element.target.classList.add('toLeftAnimation')
+
+    })
+})
+
+p2_About.forEach(element => p2_About_Observer.observe(element))
--- JavaScript/mainAnimation.js
+++ JavaScript/mainAnimation.js
@@ -1,54 +1,33 @@
-const p1=document.querySelectorAll('.intro p:nth-child(1)')
+const p1_Main = document.querySelectorAll('.intro p:nth-child(1)')
 
-const p1_Observer = new IntersectionObserver((elements)=>{
-    elements.forEach(element=>{
-        if(element.isIntersecting){
-            element.target.classList.add('letterSpacingAnimation')
+const p1_Main_Observer = new IntersectionObserver((elements) => {
+    elements.forEach(element => {
+        if (element.isIntersecting) {
+            element.target.classList.add('toRightAnimation')
+                
+            document.querySelectorAll('.listHeader div').forEach(element=>{
+                element.classList.remove('active')
+            })
             document.querySelectorAll('.listHeader div:nth-child(1)')[0].classList.add('active')
+        }
+        else {
+            document.querySelectorAll('.listHeader div:nth-child(1)')[0].classList.remove('active')
         }
     })
 })
 
-p1.forEach(element=>p1_Observer.observe(element))
+p1_Main.forEach(element => p1_Main_Observer.observe(element))
 
 
 
 
-const p2=document.querySelectorAll('.intro p:nth-child(2)')
+const p2_Main = document.querySelectorAll('.endIntro')
 
-const p2_Observer = new IntersectionObserver((elements)=>{
-    elements.forEach(element=>{
-        if(element.isIntersecting)
-            element.target.classList.add('toRightAnimation')
-    })
-})
-
-p2.forEach(element=>p2_Observer.observe(element))
-
-
-
-
-const p3=document.querySelectorAll('.intro p:nth-child(3)')
-
-const p3_Observer = new IntersectionObserver((elements)=>{
-    elements.forEach(element=>{
-        if(element.isIntersecting)
-            element.target.classList.add('toLeftAnimation')
-    })
-})
-
-p3.forEach(element=>p3_Observer.observe(element))
-
-
-
-
-const p4=document.querySelectorAll('.endIntro')
-
-const p4_Observer = new IntersectionObserver((elements)=>{
-    elements.forEach(element=>{
-        if(element.isIntersecting)
+const p2_Main_Observer = new IntersectionObserver((elements) => {
+    elements.forEach(element => {
+        if (element.isIntersecting)
             element.target.classList.add('toTopAnimation')
     })
 })
 
-p4.forEach(element=>p4_Observer.observe(element))
+p2_Main.forEach(element => p2_Main_Observer.observe(element))
